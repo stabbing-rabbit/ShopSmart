89a8f4bd90643009406f9cbf21f065a96d325149
Joseph <etoh101@gmail.com>
2020-11-05 12:26:50 -0800

    added test package

---
+++ __test__/mongodb.js
@@ -0,0 +1,17 @@
+const fs = require('fs')
+const path = require('path')
+const api = require('../server/routes/api')
+const userControl = require('../server/controllers/userController.js')
+const shopControl = require('../server/controllers/shopController.js')
+
+
+describe ('api login test', () => {
+
+  // describe('check for user fetch', => {
+
+
+
+  // })
+
+
+});
