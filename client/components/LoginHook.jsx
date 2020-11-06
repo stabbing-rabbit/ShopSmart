a40e2809c0f2a65a8cf024741a26e632769dbf0e
Joseph <etoh101@gmail.com>
2020-11-05 18:24:05 -0800

    (add frontend) added login disply and Switch&Router to Form Page

--- client/components/LoginHook.jsx
+++ client/components/LoginHook.jsx
@@ -1,36 +1,45 @@
 import React, { useState, useEffect } from 'react';
 import Logo from '../../smartShopLogo.png';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 
+
+//refactored basic components to hooks
 const LoginHook = () => {
+  // LoginHook state
   const [showLogin, setShowLogin] = useState(false);
   const [user, setUser] = useState('');
   const [password, setPassword] = useState('');
   const [submit, setSubmit] = useState(false);
+  const [userData, setUserData] = useState(null)
 
+ // when the submit button is pressed make a query to back end and verify if user exists
+ // useEffect is only activated when submit is updated
   useEffect(() => {
     if (user !== '' && password !== '') {
-      fetch('http://localhost:3000/api/createUser', {
+      fetch('api/users', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify({
           userName: user,
-          userPassword: password,
+          password: password,
         })
       })
 
-      .then((data) => data.json())
+      .then((data) => {
+        // console.log('I MADE IT TO THE FRONT END', data)
+        return data.json()
+      })
+      .then(data => {
+        setUserData(data) 
+      })
       .catch((err) => {
         if (err) {
           console.log(err);
         }
       })
     }
-
   }, [submit])
 
-  // conditional if login is clicked render a login input
-  // if continue as guest is pressed go to menu
   return (
     <div>
       <h1 className="opener">Welcome to</h1>
@@ -39,14 +48,17 @@ const LoginHook = () => {
       </h1>
       <h3 className="open">Sign In To Review Your Shopping Lists Or Continue As Guest</h3>
       <div className="intro">
+        {/* if link is clicked go directly to main page */}
         <Link className='gotomiddle' to='/FormHook' >Continue as Guest</Link>
         <button className="gotomiddle" onClick={() => setShowLogin(!showLogin)} >Sign in</button>
-
+        {/* conditional if login is clicked render a login input */}
         {showLogin &&
           <form onSubmit={(e) =>{
+              // update submit state to trigger useEffect
               e.preventDefault();
               setSubmit(!submit)
             }}>
+              {/* inputs for users */}
             <label>
               Username:
             </label>
@@ -68,6 +80,8 @@ const LoginHook = () => {
             />
             <button style={{padding:'20px'}}>Submit</button>
           </form>}
+          {/* when user data is received got to main page */}
+          {userData &&  <Redirect to="/FormHook" />}
       </div>
     </div>
   )
